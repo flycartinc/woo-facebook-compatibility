62207c7b5cae396ea89195241ba932ea324c47b3
Ilaiyaraja <ilaiyaraja221197@gmail.com>
2024-02-28 10:22:27 +0530

    Added: sh file added

---
+++ woo_facebook.sh
@@ -0,0 +1,44 @@
+echo "Woo facebook compatibility"
+current_dir="$PWD"
+composer_run(){
+  # shellcheck disable=sc2164
+  cd "$current_dir"
+  composer install --no-dev -q
+  composer update --no-dev -q
+  cd "$current_dir"
+}
+copy_folder(){
+  cd "$current_dir"
+  cd ..
+  pack_folder=$PWD"/generated_pack"
+  compressed_plugin_folder=$pack_folder"/woo-facebook-compatibility"
+  if [ -d "$pack_folder"]; then
+    rm -r "$pack_folder"
+  fi
+  mkdir "$pack_folder"
+  mkdir "$compressed_plugin_folder"
+  move_dir=("App" "vendor" "woo-facebook-compatibility.php")
+  # shellcheck disable=sc2068
+  for dir in ${move_dir[@]}; do
+    cp -r "$current_dir/$dir" "$compressed_plugin_folder/$dir"
+  done
+  cd "$current_dir"
+}
+function zip_folder(){
+  cd "$current_dir"
+  cd ..
+  pack_compress_folder=$PWD"/generated_pack"
+  cd "$pack_compress_folder"
+  zip_name="woo-facebook-compatibility"
+  rm "$zip_name".zip
+  zip -r "$zip_name".zip $zip_name -q
+  zip -d "$zip_name".zip __MACOSX/\*
+  zip -d "$zip_name".zip \*/.DS_Store
+}
+echo "Composer Run:"
+composer_run
+echo "Copy Folder:"
+copy_folder
+echo "Zip Folder:"
+zip_folder
+echo "End"
